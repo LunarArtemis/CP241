2e6fe551392ea9d25b6f54b48cc3689f0ca9e531
LunarArtemis <54983141+LunarArtemis@users.noreply.github.com>
2022-09-25 15:29:59 +0700

    Add files via upload

---
+++ LabTest/MyStack.java
@@ -0,0 +1,8 @@
+interface MyStack {
+    public void push(Object e) throws Exception;
+    public Object pop() throws Exception;
+    public Object top() throws Exception;
+    public boolean isEmpty();
+    public boolean isFull();
+    public void clear();
+}
