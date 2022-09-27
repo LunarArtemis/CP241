c76dcd5a036ae4db49056088850b453206a199a8
LunarArtemis <54983141+LunarArtemis@users.noreply.github.com>
2022-09-27 18:39:29 +0700

    Add files via upload

--- LabTest/BalanceSymTest.java
+++ LabTest/BalanceSymTest.java
@@ -1,45 +1,43 @@
 import javax.swing.JOptionPane;
 
 public class BalanceSymTest {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         // ArrayStack stack = new ArrayStack();
         LinkedStack stack = new LinkedStack();
-
-        String ex = JOptionPane.showInputDialog(null, "What is your expression?");
-        
-        ex = ex.replaceAll("\\s", "");
 
         boolean balanced = true;
         int index = 0;
         char symbol;
 
         try {
+            String ex = JOptionPane.showInputDialog(null, "Enter");
+            ex = ex.replace(" ", "");
+
             while (balanced && index < ex.length()) {
                 symbol = ex.charAt(index);
-                if (symbol == '(' || symbol == '[' || symbol == '{') {
+                if (symbol == '(' || symbol == '{' || symbol == '[') {
                     stack.push(symbol);
-                } else if (symbol == ')' || symbol == ']' || symbol == '}') {
+                } else if (symbol == ')' || symbol == '}' || symbol == ']') {
                     if (stack.isEmpty()) {
                         balanced = false;
                     } else {
                         char top = (char) stack.pop();
-                        if (symbol == ')' && top != '(' || symbol == ']' && top != '[' || symbol == '}' && top != '{') {
+                        if (symbol == ')' && top != '(' || symbol == '}' && top != '{' || symbol == ']' && top != '[') {
                             balanced = false;
                         }
                     }
                 }
                 index++;
             }
+
             if (balanced && stack.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "The expression is balanced");
-            } else if (balanced && stack.isEmpty()) {
+            } else if (!balanced && stack.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Stack is empty :Not Balancing Symbol");
-            }
-            else if (balanced && !stack.isEmpty()) {
+            } else if (balanced && !stack.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Illegal Expression : Not Balancing Symbol");
-            }
-            else {
+            } else {
                 JOptionPane.showMessageDialog(null, "The expression is not balanced at position " + index);
             }
         } catch (Exception e) {
