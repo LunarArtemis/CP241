979a87710823125b403a5c9d7cf71eb985cde77f
LunarArtemis <54983141+LunarArtemis@users.noreply.github.com>
2022-09-27 22:42:17 +0700

    Add files via upload

--- LabTest/BalanceSymTest.java
+++ LabTest/BalanceSymTest.java
@@ -6,8 +6,8 @@ public class BalanceSymTest {
         // ArrayStack stack = new ArrayStack();
         LinkedStack stack = new LinkedStack();
 
-        boolean balanced = true;
-        int index = 0;
+        boolean balanced  = true;
+        int index  =0;
         char symbol;
 
         try {
@@ -21,27 +21,27 @@ public class BalanceSymTest {
                 } else if (symbol == ')' || symbol == '}' || symbol == ']') {
                     if (stack.isEmpty()) {
                         balanced = false;
+                        throw new Exception("Stack is empty :Not Balancing Symbol");
                     } else {
                         char top = (char) stack.pop();
                         if (symbol == ')' && top != '(' || symbol == '}' && top != '{' || symbol == ']' && top != '[') {
                             balanced = false;
+                            throw new Exception( "The expression is not balanced at position " + (index + 1));
                         }
                     }
                 }
                 index++;
             }
 
+
             if (balanced && stack.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "The expression is balanced");
-            } else if (!balanced && stack.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Stack is empty :Not Balancing Symbol");
             } else if (balanced && !stack.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Illegal Expression : Not Balancing Symbol");
-            } else {
-                JOptionPane.showMessageDialog(null, "The expression is not balanced at position " + index);
             }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
+
+        }catch(Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
         }
     }
 }
